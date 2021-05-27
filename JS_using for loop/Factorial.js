509d3ef42339ab733540cf656428db40e5c67350
SHEKHAR240196 <80879927+SHEKHAR240196@users.noreply.github.com>
2021-05-28 00:03:28 +0530

    [Shekhar] Add: Factorial program is added

--- JS_using for loop/Factorial.js
+++ JS_using for loop/Factorial.js
@@ -2,7 +2,7 @@ const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+}); 
 
 rl.question("Enter a Number ? ", function(number) {
        let result = 1;
