30865f51897e9d68bf5d4a1fa058a7d546a3b2d3
SHEKHAR240196 <80879927+SHEKHAR240196@users.noreply.github.com>
2021-05-27 23:40:08 +0530

    [Shekhar] Add: MinMax program is added

--- JS_using if & else/MinMax.js
+++ JS_using if & else/MinMax.js
@@ -1,9 +1,10 @@
 let n = 1;
 let max = Math.floor(100+ ((Math.random() * 10) % 900));
 let min = max;
+
 console.log(max);
 
-while(n <  4) {
+while (n <  4) {
     randomNumber = Math.floor(100 + ((Math.random() * 10) % 900));
     console.log(randomNumber);
     if (max < randomNumber) {
